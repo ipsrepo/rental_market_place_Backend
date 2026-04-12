7369dd30fd5bf0f8e8cfd2da89f3f48a9ac53e2d
Prakash <prakaships@outlook.com>
2026-04-12 17:24:17 +0100

    - Removed unnecessary code from protected routes

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ process.on('uncaughtException', (err) => {
 
 const app = require('./app');
 
+//
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD,
+// );
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
-
-// const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE_LOCAL;
 console.log('🔍 DB string:', DB);
 
 mongoose
